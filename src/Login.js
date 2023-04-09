49f9993de0a906969241722a9bfb416eb5d3efa2
NgalotJ <ngalot.johnvictor@gmail.com>
2023-04-09 23:02:41 +0800

    Documenting

--- src/Login.js
+++ src/Login.js
@@ -6,18 +6,24 @@ import './Login.css';
 export class Login extends Component{
     state = {
         passwordShown: false,
-        inputValue: "",
+        usernameFieldValue: "",
     };
 
-    handleUserInput = (e) => {
-        this.setState({inputValue: e.target.value});
+    // Input Username Field handler
+    handleUsernameInput = (e) => {
+        this.setState({usernameFieldValue: e.target.value});
     };
 
-    resetInputField = () => {
-        this.setState({inputValue: ""})
+    // Reset Input Username Field handler
+    clearUsernameField = () => {
+        this.setState({usernameFieldValue: ""})
     }
 
-    togglePassword = () => {
+    /**
+     * toggles the passwordShown state value using setState
+     * to update the state based on the previous state
+     */
+    togglePasswordVisibility = () => {
         this.setState((prevState) => ({
             passwordShown: !prevState.passwordShown,
         }));
@@ -29,7 +35,7 @@ export class Login extends Component{
     }
 
     render(){
-        const {passwordShown, inputValue} = this.state;
+        const {passwordShown, usernameFieldValue} = this.state;
 
         return(
             <div>
@@ -44,8 +50,8 @@ export class Login extends Component{
                                     <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/>
                                 </svg>
                             </InputGroup.Text>
-                            <Form.Control type='text' id='username' value={inputValue} onChange={this.handleUserInput}></Form.Control>
-                            <Button variant='danger' className='btn-inline-form' onClick={this.resetInputField}>
+                            <Form.Control type='text' id='username' value={usernameFieldValue} onChange={this.handleUsernameInput}></Form.Control>
+                            <Button variant='danger' className='btn-inline-form' onClick={this.clearUsernameField}>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
                                     <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
                                 </svg>
@@ -61,7 +67,7 @@ export class Login extends Component{
                                 </svg>
                             </InputGroup.Text>
                             <Form.Control type={passwordShown ? 'text' : 'password'} id='password'></Form.Control>
-                            <Button variant='danger' className='btn-inline-form' onClick={this.togglePassword}>
+                            <Button variant='danger' className='btn-inline-form' onClick={this.togglePasswordVisibility}>
                                 {passwordShown ? 
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
                                         <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
@@ -73,7 +79,7 @@ export class Login extends Component{
                                         <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829z"/>
                                         <path d="M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12-.708.708z"/>
                                     </svg>
-                                }
+                                } 
                             </Button>
                         </InputGroup>
                     </Form.Group>
